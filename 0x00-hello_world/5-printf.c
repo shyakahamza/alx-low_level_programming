4b81dfc0e9a2b9c953bdd88689c428dfc6433211
Shyaka <shyakahamza@gmail.com>
2023-03-20 03:11:09 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-/** main - A program that prints a line using the printf function
+/**
+ * main - A program that prints a line using the printf function
  * Return: 0
  */
 int main (void)
